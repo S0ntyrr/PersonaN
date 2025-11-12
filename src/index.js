b32a8bd91b6ef3cbfd60fa1c381315c719fa4808
S0ntyrr <100627317+S0ntyrr@users.noreply.github.com>
2025-11-12 09:22:12 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import morgan from 'morgan';
+import {engine} from "express-handlebars";
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
+import personasRoutes from './routes/personas.routes.js';
+
+//Initializations
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//Settings
+app.set('port', process.env.PORT || 3306);
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({ 
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.use(personasRoutes);
+
+//public files
+app.use(express.static(join(__dirname, 'public')));
+
+//Starting the server
+app.listen(app.get('port'), () => 
+    console.log('El server esta escuchando en el puerto', app.get('port')));
